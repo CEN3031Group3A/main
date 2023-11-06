954343abc6ca7f67d2f9d923957eb62dc35bd33a
Hoang Le Duc <hoangleduc@Hoangs-MacBook-Pro.local>
2023-11-06 17:46:08 -0500

    added search bar

--- client/src/views/Gallery/Gallery.jsx
+++ client/src/views/Gallery/Gallery.jsx
@@ -7,39 +7,52 @@ import './Gallery.less'
 
 const Gallery = () => {
 
-
     const [filterOptions, setFilterOptions] = useState(false);
+    const [searchTerm, setSearchTerm] = useState(""); // state to hold the search term
 
     const toggleOptions = () => {
         setFilterOptions(!filterOptions);
     };
 
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value); // updates the state with the input from the search bar
+    };
+
+    // Implement search functionality as needed
+    const handleSearch = () => {
+        // Search logic here
+    };
     return (
         <div className='container nav-padding'>
-            <NavBar />
-            <div id='join-wrapper'>
-                <h1 style={{color:"#FFFFFF"}}>GALLERY</h1>
-                <img onClick={toggleOptions} src={Filter} id='filter' className='filterImage' alt='filter'/>
-                    {filterOptions && (
-                        <div className='option-list'>
-                            <h1 className='option-title'>View Options</h1>
-                            <label>
-                                <input type='checkbox' />Classroom
-                            </label>
-                            <label>
-                                <input type='checkbox' />Organizational
-                            </label>
-                            <label>
-                                <input type='checkbox' />Public
-                            </label>
-                        </div>
-                    )}
+        <NavBar />
+        <div id='gallery-title'>
+          <h1 style={{color:"#FFFFFF"}}>GALLERY</h1>
+        </div>
+        <div id='join-wrapper'>
+          <div id="search-wrapper">
+            <input type="text" className="search-input" placeholder="Search..." />
+          </div>
+          <img onClick={toggleOptions} src={Filter} className='filterImage' alt='filter'/>
+          {filterOptions && (
+            <div className='option-list'>
+                        <h1 className='option-title'>View Options</h1>
+                        <label>
+                            <input type='checkbox' />Classroom
+                        </label>
+                        <label>
+                            <input type='checkbox' />Organizational
+                        </label>
+                        <label>
+                            <input type='checkbox' />Public
+                        </label>
+                    </div>
+                )}
+                
                 <img src={Add} id='add' className='add-image' alt='add'/>
             </div>
-
-
-        </div>
+        </div> 
     )
 };
 
 export default Gallery;
+
