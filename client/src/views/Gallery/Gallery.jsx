53fddfdc39c8073e39674861aea1bf338e3e8eca
sensen312 <sensen312school@gmail.com>
2023-11-06 19:40:41 -0500

    ADDED GRID

--- client/src/views/Gallery/Gallery.jsx
+++ client/src/views/Gallery/Gallery.jsx
@@ -3,41 +3,47 @@ import Filter from "../../assets/filter.png";
 import Add from "../../assets/add_button.png";
 
 import NavBar from "../../components/NavBar/NavBar";
+
+import GallerySelection from './GallerySelection';
+
 import './Gallery.less'
 
 const Gallery = () => {
 
 
     const [filterOptions, setFilterOptions] = useState(false);
+    const [posts, setPosts] = useState([]);
 
     const toggleOptions = () => {
         setFilterOptions(!filterOptions);
     };
 
+
+
     return (
         <div className='container nav-padding'>
             <NavBar />
             <div id='join-wrapper'>
-                <h1 style={{color:"#FFFFFF"}}>GALLERY</h1>
-                <img onClick={toggleOptions} src={Filter} id='filter' className='filterImage' alt='filter'/>
-                    {filterOptions && (
-                        <div className='option-list'>
-                            <h1 className='option-title'>View Options</h1>
-                            <label>
-                                <input type='checkbox' />Classroom
-                            </label>
-                            <label>
-                                <input type='checkbox' />Organizational
-                            </label>
-                            <label>
-                                <input type='checkbox' />Public
-                            </label>
-                        </div>
-                    )}
-                <img src={Add} id='add' className='add-image' alt='add'/>
+                <h1 style={{ color: "#FFFFFF" }}>GALLERY</h1>
+                <img onClick={toggleOptions} src={Filter} id='filter' className='filterImage' alt='filter' />
+                {filterOptions && (
+                    <div className='option-list'>
+                        <h1 className='option-title'>View Options</h1>
+                        <label>
+                            <input type='checkbox' />Classroom
+                        </label>
+                        <label>
+                            <input type='checkbox' />Organizational
+                        </label>
+                        <label>
+                            <input type='checkbox' />Public
+                        </label>
+                    </div>
+                )}
+                <img src={Add} id='add' className='add-image' alt='add' />
             </div>
 
-
+            <GallerySelection posts={posts} /> {/* Use GallerySelection component */}
         </div>
     )
 };
