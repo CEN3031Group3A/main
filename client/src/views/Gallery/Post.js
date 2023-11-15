864f90415a3d675f0d9c28b1dc32dd0d40d4526f
gabeisenberg <g.isenberg@ufl.edu>
2023-11-15 15:40:15 -0500

    Random Post

---
+++ client/src/views/Gallery/Post.js
@@ -0,0 +1,23 @@
+const faker = require('faker');
+
+function randomPost() {
+    const post = {
+        firstName: faker.options.firstName(),
+        lastName: faker.options.lastName(),
+        image: faker.image.transport(),
+        views: faker.number.int({
+            min: 0,
+            max: 100
+        }),
+        likes: faker.number.int({
+            min: 0,
+            max: 100
+        }),
+        saved: faker.random.boolean()
+    };
+    return post;
+}
+
+module.exports = getRandomPost;
+
+
