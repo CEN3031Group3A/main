61b7d0dab5d35e0fb9f0972344024790d3e92463
David-J-Rogers <143656714+David-J-Rogers@users.noreply.github.com>
2023-12-07 14:59:07 -0500

    added comments

--- client/src/components/GalleryCards/likeButton.jsx
+++ client/src/components/GalleryCards/likeButton.jsx
@@ -5,13 +5,16 @@ import Unliked from "../../assets/unliked.png";
 const LikeButton = ({ post, updatePost }) => {
   const [liked, setLiked] = useState(post.liked);
 
+  // handling the like button click
   const handleLike = () => {
+    // toggle the liked state
     const updatedLiked = !liked;
     setLiked(updatedLiked);
 
+    // updated like count based on the new liked state
     const updatedLikeCount = updatedLiked ? post.likes + 1 : post.likes - 1;
 
-    console.log(`Post ID ${post.id} Liked: ${updatedLiked}`);
+    // call the updatePost function to update the post with new liked state and like count
     updatePost(post.id, { ...post, liked: updatedLiked, likes: updatedLikeCount });
   };
 
@@ -19,13 +22,23 @@ const LikeButton = ({ post, updatePost }) => {
 
   const buttonStyle = {
     display: 'flex',
-    alignItems: 'center', 
+    alignItems: 'center',
     cursor: 'pointer',
   };
 
   return (
     <div style={buttonStyle}>
-      <button style={{ width: '40px', height: '40px', border: 'none', backgroundImage: `url(${imageSource})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundColor: 'transparent', cursor: 'pointer' }} onClick={handleLike}></button>
+      <button style={{ 
+        width: '40px',
+        height: '40px',
+        border: 'none',
+        backgroundImage: `url(${imageSource})`,
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor: 'transparent',
+        cursor: 'pointer'
+      }} onClick={handleLike}></button>
+
       <span style={{ marginLeft: '0px', color: '#fff' }}>{post.likes}</span>
     </div>
   );
